7c3e7dfb117911cb578dd1a489d7dfbd90171895
depravo <sorokaa6102@gmail.com>
2021-03-22 23:19:08 +0300

    lab_Durak

---
+++ c#/Durak/Durak/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Durak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonYes = new System.Windows.Forms.Button();
+            this.buttonNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.No;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(155, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Are you foolish?";
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // buttonYes
+            // 
+            this.buttonYes.Location = new System.Drawing.Point(241, 153);
+            this.buttonYes.Name = "buttonYes";
+            this.buttonYes.Size = new System.Drawing.Size(87, 49);
+            this.buttonYes.TabIndex = 1;
+            this.buttonYes.TabStop = false;
+            this.buttonYes.Text = "Yes";
+            this.buttonYes.UseVisualStyleBackColor = true;
+            this.buttonYes.Click += new System.EventHandler(this.button_Yes_Click);
+            this.buttonYes.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // buttonNo
+            // 
+            this.buttonNo.Location = new System.Drawing.Point(431, 153);
+            this.buttonNo.Name = "buttonNo";
+            this.buttonNo.Size = new System.Drawing.Size(87, 49);
+            this.buttonNo.TabIndex = 2;
+            this.buttonNo.TabStop = false;
+            this.buttonNo.Text = "No";
+            this.buttonNo.UseVisualStyleBackColor = true;
+            this.buttonNo.Click += new System.EventHandler(this.button_No_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.buttonNo);
+            this.Controls.Add(this.buttonYes);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Are you fool?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonYes;
+        private System.Windows.Forms.Button buttonNo;
+    }
+}
+
---
+++ c#/Durak/Durak/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Durak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button_Yes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("My condolences!");
+            this.Close();
+        }
+
+        private void button_No_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Does not look like it!");
+            this.Close();
+        }
+        string cheatcode = "";
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = MousePosition.X - this.Location.X;
+            int y = MousePosition.Y - this.Location.Y;
+            if (cheatcode == "putin") 
+            {
+                return;
+            }
+            checkMousePosition(x, y);
+        }
+
+        private void checkMousePosition(int x, int y)
+        {
+            if (buttonNo.Location.X - 20 <= x && x <= buttonNo.Location.X + buttonNo.Width + 20)
+            {
+                if (y > buttonNo.Location.Y)
+                {
+                    if (y - buttonNo.Location.Y - buttonNo.Height < 50)
+                    {
+                        if (buttonNo.Location.Y > 25)
+                        {
+                            buttonNo.Location = new Point(buttonNo.Location.X, buttonNo.Location.Y - 25);
+                        }
+                        else
+                        {
+                            buttonNo.Location = new Point(buttonNo.Location.X, this.Size.Height - buttonNo.Height - 60);
+                        }
+                    }
+                }
+                else
+                {
+                    if (buttonNo.Location.Y - y < 50)
+                    {
+                        if (buttonNo.Location.Y + buttonNo.Height + 50 < this.Size.Height)
+                        {
+                            buttonNo.Location = new Point(buttonNo.Location.X, buttonNo.Location.Y + 25);
+                        }
+                        else
+                        {
+                            buttonNo.Location = new Point(buttonNo.Location.X, 40);
+                        }
+                    }
+                }
+            }
+            if (buttonNo.Location.Y - 20 <= y && y <= buttonNo.Location.Y + buttonNo.Height + 20)
+            {
+                if (x < buttonNo.Location.X)
+                {
+                    if (buttonNo.Location.X - x <= 50)
+                    {
+                        if (buttonNo.Location.X + buttonNo.Width + 50 <= this.Size.Width)
+                        {
+                            buttonNo.Location = new Point(buttonNo.Location.X + 25, buttonNo.Location.Y);
+                        }
+                        else
+                        {
+                            buttonNo.Location = new Point(40, buttonNo.Location.Y);
+                        }
+                    }
+                }
+                else
+                {
+                    if (x - (buttonNo.Location.X + buttonNo.Width) <= 50)
+                    {
+                        if (buttonNo.Location.X >= 50)
+                        {
+                            buttonNo.Location = new Point(buttonNo.Location.X - 25, buttonNo.Location.Y);
+                        }
+                        else
+                        {
+                            buttonNo.Location = new Point(this.Size.Width - buttonNo.Width - 60, buttonNo.Location.Y);
+                        }
+                    }
+                }
+            }
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar == (char)Keys.Delete || e.KeyChar == (char)Keys.Back) && cheatcode.Length > 0)
+                cheatcode = cheatcode.Substring(0, cheatcode.Length - 1);
+            else
+                cheatcode += e.KeyChar.ToString();
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                case Keys.Down:
+                case Keys.Left:
+                case Keys.Right:
+                    e.IsInputKey = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = MousePosition.X - this.Location.X;
+            int y = MousePosition.Y - this.Location.Y;
+            checkMousePosition(x, y);
+
+        }
+    }
+}
